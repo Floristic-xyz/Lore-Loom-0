dd3ebd40c272807ec14b2a58eec03bf67fbf47b2
Kwesimodo <162521734+Kwesimodo@users.noreply.github.com>
2024-03-16 22:12:34 +0000

    Add files via upload
    working ver

--- listAttestations.js
+++ listAttestations.js
@@ -1,17 +1,25 @@
 import axios from 'axios';
 
+// this async function will check the schema ID for all the attestations within its UID and return them. 
 async function listAttestations() {
     try {
         const response = await axios.post('https://base.easscan.org/graphql', {
             query: `
-                query Attestations {
-                    attestations(take: 25, orderBy: {time: desc}) {
-                        id
-                     
-                    }
+            query ExampleQuery($attestationsWhere2: AttestationWhereInput) {
+                attestations(where: $attestationsWhere2) {
+                    attester
+                    id
                 }
-            `
-        });
+            }
+        `,
+        variables: {
+            attestationsWhere2: {
+                schemaId: {
+                    equals: "0x7f0c06286c4ec7eed415a6c76bf57093f5ed14eba6aafe75ce09cb68d1d73b79"
+                }
+            }
+        }
+    });
 
         console.log(JSON.stringify(response.data));
     } catch (error) {
